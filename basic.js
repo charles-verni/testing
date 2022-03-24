9cf2aee2bce6ef6d24b0c5a67a9c90f82a538a95
charles-verni <charles.verni@gmail.com>
2022-03-24 20:34:37 +0000

    init

---
+++ basic.js
@@ -0,0 +1,7 @@
+let example1 = "Hello! <br/>";
+document.write(example1);
+let example2 = 1 + 1 + "<br/>";
+document.write(example2)
+let example3 = "THIS IS AN EXAMPLE STRING <br/>";
+document.write(example3)
+let lower = document.write(example3.toLowerCase());
